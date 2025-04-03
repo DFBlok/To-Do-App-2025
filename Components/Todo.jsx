b9d6211d4c19b444f4fb4c3e507c6fe579eda68e
DFBlok <s219392862@mandela.ac.za>
2025-04-03 08:25:05 +0200

    fixed spelling errors

--- Components/Todo.jsx
+++ Components/Todo.jsx
@@ -16,7 +16,7 @@ const Todo = ({id, title, description, mongoId, complete, deleteTodo, completeTo
                     {complete?"Completed":"Pending"}
                 </td>
                 <td className="px-6 py-4 flex gap-2">
-                    <button onClick={()=>deleteTodo(mongoId)} className="py-2 px-4 bg-red-500 text-white " >Delte</button>
+                    <button onClick={()=>deleteTodo(mongoId)} className="py-2 px-4 bg-red-500 text-white " >Delete</button>
                     {complete?"":<button onClick={()=>completeTodo(mongoId)} className='py-2 px-4 bg-green-500 text-white'>Done</button>}
                 </td>
             </tr>
